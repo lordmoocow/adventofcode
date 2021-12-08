3432737fab686b62e6c4224fdd3a902767fa4544
Sam Williams <src@samueljwilliams.co.uk>
2021-12-08 00:57:17 +0000

    day 1 part 2

--- 2021/1/src/main.rs
+++ 2021/1/src/main.rs
@@ -1,20 +1,37 @@
+use itertools::multizip;
 use std::fs::File;
-use std::io::{BufReader, BufRead, Error};
+use std::io::{BufRead, BufReader, Error};
 
 fn main() -> Result<(), Error> {
     let input = File::open("input")?;
     let reader = BufReader::new(input);
 
+    // Read input and parse lines to ints
+    let data: Vec<_> = reader
+        .lines()
+        .into_iter()
+        .map(|x| x.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    // Get slices of data with an offset for each window
+    let a = &data[..];
+    let b = &data[1..];
+    let c = &data[2..];
+
     let mut count = 0;
-    let mut previous_measurement = 0;
-    reader.lines().into_iter().skip(1).for_each(|raw_value| {
-        if let Ok(value) = raw_value.unwrap().parse() {
-            if value > previous_measurement {
+    let mut previous_window: Option<i32> = None;
+
+    // Zip 'em all together
+    for (x, y, z) in multizip((a, b, c)) {
+        let window = x + y + z;
+        // If we have a previous value to compare, i.e. not the first window, then do so
+        if let Some(previous_window) = previous_window {
+            if window > previous_window {
                 count += 1;
             }
-            previous_measurement = value;
-        };
-    });
+        }
+        previous_window.replace(window);
+    }
 
     println!("{}", count);
 
