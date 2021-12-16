13cbfed15b8ef01066bf625318c1a29df91d756a
Sam Williams <src@samueljwilliams.co.uk>
2021-12-16 22:04:01 +0000

    day 8 part 2

--- 2021/8/src/main.rs
+++ 2021/8/src/main.rs
@@ -4,17 +4,83 @@ use std::io::{BufRead, BufReader, Error};
 fn main() -> Result<(), Error> {
     let signals = read_input("/workspaces/advent/2021/8/input")?;
 
-    //println!("{:?}", &signals);
     println!("{:?}", part1(&signals));
-    // println!("Fuel 2: {:?}", part2(&crabs));
+    println!("{:?}", part2(&signals));
 
     Ok(())
 }
 
 fn part1(signals: &[Signal]) -> usize {
     signals.iter().fold(0, |total, s| {
-        total + s.output.iter().filter(|x| (x.len() >= 2 && x.len() <= 4) || x.len() == 7).count()
+        total
+            + s.output
+                .iter()
+                .filter(|x| (x.len() >= 2 && x.len() <= 4) || x.len() == 7)
+                .count()
     })
+}
+
+fn part2(signals: &[Signal]) -> usize {
+    let mut tmp = vec![vec![' '; 0]; 11];
+    signals.iter().fold(0, |total, s| {
+        // determine known input patterns for this signal
+        for input in &s.input {
+            match input.len() {
+                2 => tmp[1] = input.chars().collect(),
+                3 => tmp[7] = input.chars().collect(),
+                4 => tmp[4] = input.chars().collect(),
+                7 => tmp[8] = input.chars().collect(),
+                _ => (),
+            };
+        }
+        // get output value
+        let display = &s
+            .output
+            .iter()
+            .map(|digit| digit.chars().collect())
+            .map(|digit: Vec<char>| match digit.len() {
+                2 => 1,
+                3 => 7,
+                4 => 4,
+                5 => {
+                    if slice_contains(&digit, &tmp[1]) {
+                        3
+                    } else if slice_remains(&digit, &tmp[4]) == 1 {
+                        5
+                    } else {
+                        2
+                    }
+                }
+                6 => {
+                    if slice_contains(&digit, &tmp[4]) {
+                        9
+                    } else if slice_contains(&digit, &tmp[1]) {
+                        0
+                    } else {
+                        6
+                    }
+                }
+                7 => 8,
+                _ => 0_usize,
+            })
+            // concat the array of digits into a 4 digit int
+            .fold(0, |acc, i| acc * 10 + i);
+        total + display
+    })
+}
+
+fn slice_contains(a: &[char], b: &[char]) -> bool {
+    for x in b {
+        if !a.contains(x) {
+            return false;
+        }
+    }
+    true
+}
+
+fn slice_remains(a: &[char], b: &[char]) -> usize {
+    b.iter()
+        .fold(0, |total, c| total + if a.contains(c) { 0 } else { 1 })
 }
 
 fn read_input(path: &str) -> Result<Vec<Signal>, Error> {
