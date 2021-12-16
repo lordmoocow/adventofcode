6b8d8b1b48f07ed9a7f37309c88ca4015bffc517
Sam Williams <src@samueljwilliams.co.uk>
2021-12-16 08:03:24 +0000

    day 7 part 2

--- 2021/7/src/main.rs
+++ 2021/7/src/main.rs
@@ -6,33 +6,42 @@ fn main() -> Result<(), Error> {
     let crabs = read_input("/workspaces/advent/2021/7/input")?;
     let crabs = count(&crabs);
 
-    println!("Crabs: {:?}", &crabs);
-    println!("Fuel: {:?}", calc(&crabs));
+    //println!("Crabs: {:?}", &crabs);
+    println!("Fuel 1: {:?}", part1(&crabs));
+    println!("Fuel 2: {:?}", part2(&crabs));
 
     Ok(())
 }
 
-fn calc(crabs: &HashMap<usize,usize>) -> usize {
-    let mut fuel_costs = HashMap::new();
-    for pos in 0..*crabs.keys().max().unwrap() {
-        fuel_costs.insert(pos, 0);
-    }
+fn part1(crabs: &HashMap<usize, usize>) -> usize {
+    calc(crabs, |n| n )
+}
 
-    for (target, fuel) in fuel_costs.iter_mut() {
-        for (pos, submarines) in crabs.iter() {
-            let steps = if pos > target {
-                *pos - *target
+fn part2(crabs: &HashMap<usize, usize>) -> usize {
+    calc(crabs, |n| n * (n + 1) / 2  )
+}
+
+fn calc<F>(crabs: &HashMap<usize, usize>, fuel_cost: F) -> usize
+where
+    F: Fn(usize) -> usize,
+{
+    let mut fuel_costs = vec![0; *crabs.keys().max().unwrap()];
+
+    for (target, fuel) in fuel_costs.iter_mut().enumerate() {
+        for (pos, count) in crabs.iter() {
+            let steps = if pos > &target {
+                pos - target
             } else {
-                *target - *pos
+                target - pos
             };
-            *fuel += submarines * steps;
+            *fuel += count * fuel_cost(steps);
         }
     }
 
-    *fuel_costs.values().min().unwrap()
+    *fuel_costs.iter().min().unwrap()
 }
 
-fn count(crabs: &[usize]) -> HashMap<usize,usize> {
+fn count(crabs: &[usize]) -> HashMap<usize, usize> {
     let mut counts = HashMap::new();
     for crab in crabs {
         *counts.entry(*crab).or_insert(0) += 1;
